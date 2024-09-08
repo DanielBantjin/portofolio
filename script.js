686fac3823c6bfc41f6ca3fcca67f64e45c35572
DanielBantjin <danielseptianveribancin@gmail.com>
2024-09-09 02:29:30 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('calculator').addEventListener('click', function() {
+    window.location.href = 'https://github.com/DanielBantjin/Calculator';
+});
+document.getElementById('web').addEventListener('click', function() {
+    window.location.href = 'https://github.com/DanielBantjin/company-web-profile';
+});
+document.getElementById('ig').addEventListener('click', function() {
+    window.location.href = 'https://www.instagram.com/danielbantjin/';
+});
+document.getElementById('fb').addEventListener('click', function() {
+    window.location.href = 'https://www.facebook.com/bantjin?locale=id_ID';
+});
+document.getElementById('ld').addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/daniel-bancin-08374017a/';
+});
+document.getElementById('wa').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/081343932525';
+});
+
+
